e67ba41b80cee764fb898b5998b77bcf03aac3e7
KSentak <k.sentak@outlook.com>
2019-11-08 14:30:14 -0500

    Added pseudocode to liri.js

--- liri.js
+++ liri.js
@@ -2,3 +2,36 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+var axios = require("axios");
+
+//Capture command that the user inputs
+
+var userInput = process.argv[2];
+var userRequest = //Everything after process.argv[2]
+
+console.log(userInput);
+console.log(userRequest);
+
+//Use switch or if statemnts
+
+//Check if user input is "concert-this"
+//Run API call to bands in town api
+//Inject users search into the query URL
+//Display name of venue, venue location, date of event
+//Use moment to format date
+
+//=================================================================================================================
+
+//Check if user input is "spotify-this-song"
+//Using spotify node package, make a call to spotify API
+//Display song name, a preview link, the album
+//Provide a defeault if no song is provided
+
+//===================================================================================================================
+
+//Check if user input is "movie-this"
+
+//=====================================================================================================================
+
+//Check if user input is "do-what-it-says"
+
