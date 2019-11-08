9a9fe13aa147a7918e61d134d467854d38ba394e
KSentak <k.sentak@outlook.com>
2019-11-08 17:11:05 -0500

    Finished movie-this section

--- liri.js
+++ liri.js
@@ -1,18 +1,54 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-var spotify = require("node-spotify-api");
-var spotify = new Spotify(keys.spotify);
+// var spotify = require("node-spotify-api");
+// var spotify = new Spotify(keys.spotify);
 var axios = require("axios");
 
 //Capture command that the user inputs
 
-var userInput = process.argv[2];
-var userRequest = //Everything after process.argv[2]
+var userCommand = process.argv[2];
+var userSearch = process.argv[3];
 
-console.log(userInput);
-console.log(userRequest);
+console.log(userCommand);
+console.log(userSearch);
 
-//Use switch or if statemnts
+//Switch between movie, spotify, concert, and do what it says
+switch (userCommand) {
+    //Use omdb api to "movie-this"
+    case "movie-this":
+        axios.get("http://www.omdbapi.com/?t="+userSearch+"&y=&plot=short&apikey=trilogy").then(
+            function (movieResponse) {
+                console.log("\n Title: "+movieResponse.data.Title);
+                console.log("\n Release Year: "+movieResponse.data.Year);
+                console.log("\n IMDB Rating: "+movieResponse.data.imdbRating);
+                // console.log("\n Rotten Tomatoes Rating: "+movieResponse.data.Title);
+                console.log("\n Country: "+movieResponse.data.Country);
+                console.log("\n Language: "+movieResponse.data.Language);
+                console.log("\n Plot: "+movieResponse.data.Plot);
+                console.log("\n Actors: "+movieResponse.data.Actors);
+                //Current rotten tomatoes rating not clean!!
+                console.log(movieResponse.data.Ratings[1]);
+            })
+        break;
+
+    case "concert-this":
+        outputNum = parseFloat(num1) - parseFloat(num2);
+        break;
+
+    case "spotify-this-song":
+        outputNum = parseFloat(num1) * parseFloat(num2);
+        break;
+
+    case "do-what-it-says":
+        outputNum = parseFloat(num1) / parseFloat(num2);
+        break;
+
+    default:
+        var unrecognized = "Not a recognized command";
+        console.log(unrecognized)
+}
+
+
 
 //Check if user input is "concert-this"
 //Run API call to bands in town api
@@ -28,10 +64,6 @@ console.log(userRequest);
 //Provide a defeault if no song is provided
 
 //===================================================================================================================
-
-//Check if user input is "movie-this"
-
-//=====================================================================================================================
 
 //Check if user input is "do-what-it-says"
 
